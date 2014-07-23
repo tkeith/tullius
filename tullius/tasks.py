566a2918cbd5da0535a9b1268be00e767cfec970
Trevor Keith <tk@tkeith.com>
2014-07-23 11:37:55 -0400

    Add logging

--- tullius/tasks.py
+++ tullius/tasks.py
@@ -9,6 +9,10 @@ import time
 import numbers
 import multiprocessing
 import threading
+import logging
+import traceback
+
+log = logging.getLogger(__name__)
 
 def ensure_indexes():
     utils.mongo_retry(lambda: get_db().tasks.ensure_index([('status', pymongo.ASCENDING), ('start_after', pymongo.ASCENDING)]))
@@ -85,36 +89,52 @@ def done_task(task, id, status, next_tasks):
 
 def process_task(task_processes_avail):
     for sem, min_priority, max_priority in task_processes_avail:
+        log.debug('trying to acquire sem for min {} max {}'.format(min_priority, max_priority))
         if sem.acquire(block=False):
+            log.debug('acquired semaphore')
             db_task = utils.mongo_retry(lambda: get_db().tasks.find_one({'status': 'queued', 'start_after': {'$lte': datetime.now()}, 'priority': {'$gte': min_priority, '$lte': max_priority}}, sort=[('priority', pymongo.ASCENDING), ('start_after', pymongo.ASCENDING)]))
             if db_task is None:
+                log.debug('no task to process, releasing sem')
                 sem.release()
+                log.debug('sem released')
             else:
+                log.debug('found task to process')
                 break
+        else:
+            log.debug('did not acquire semaphore')
     else:
         return False
 
     id = db_task['_id']
 
+    log.debug('trying to lock task')
     res = utils.mongo_retry(lambda: get_db().tasks.update({'_id': id, 'status': 'queued'}, {'$set': {'status': 'running', 'timeout_time': datetime.now() + timedelta(seconds=db_task['timeout'])}}))
     if res['n'] == 0:
+        log.debug('another instance got the task first, releasing sem')
         # Another instance got to this task before us
         sem.release()
+        log.debug('sem released')
         return True
 
     def process_db_task():
         task = Task.from_db(db_task)
 
+        log.info('processing task: {}'.format(task))
+
         try:
             next_tasks = utils.ensure_list(utils.call_in_process(task.run, timeout=task.timeout.total_seconds()))
         except Exception:
+            log.info('task {} failed: {}'.format(task, traceback.format_exc()))
+            log.debug('running failure method')
             next_tasks = task_failed(task)
+            log.debug('done failure method')
             status = 'failed'
         else:
+            log.info('task succeeded: {}'.format(task))
             status = 'done'
 
         done_task(task, id, status, next_tasks)
-
+        log.debug('releasing sem')
         sem.release()
 
     proc = multiprocessing.Process(target=process_db_task)
@@ -136,9 +156,11 @@ def handle_dead_task():
 
     def process_db_task():
         task = Task.from_db(db_task)
+        log.info('handling dead task: {}'.format(task))
         id = db_task['_id']
         next_tasks = task_failed(task)
         done_task(task, id, 'failed', next_tasks)
+        log.info('done handling dead task: {}'.format(task))
 
     utils.call_in_process(process_db_task)
 
@@ -149,14 +171,20 @@ def handle_pending_task():
     if db_task is None:
         return False
 
+    log.debug('handling pending task: {}'.format(db_task['class']))
     insert_db_tasks(db_task['next_tasks'])
     utils.mongo_retry(lambda: get_db().tasks.update({'_id': db_task['_id']}, {'$set': {'status': db_task['next_status']}, '$unset': {'next_tasks': '', 'next_status': ''}}))
+    log.debug('done handling pending task: {}'.format(db_task['class']))
 
     return True
 
 def daemon():
+    log.info('daemon starting')
+    log.info('ensuring indexes')
     ensure_indexes()
+    log.info('done ensuring indexes. creating semaphores')
     task_processes_avail = [(multiprocessing.Semaphore(num), min_priority, max_priority) for (num, min_priority, max_priority) in task_processes]
+    log.info('semaphores created. beginning main loop')
     while True:
         if not (handle_dead_task() or handle_pending_task() or process_task(task_processes_avail)):
             time.sleep(0.1)
